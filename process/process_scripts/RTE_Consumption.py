3acaa3e57279abe8b5aa25095ed4f40eff4752ca
Raphael Thireau <r.thireau@verso.energy>
2025-01-16 09:35:17 +0100

    Correction + ENEDIS Temp

--- process/process_scripts/RTE_Consumption.py
+++ process/process_scripts/RTE_Consumption.py
@@ -18,13 +18,30 @@ def RTE_Consumption_process(date, country, dir, DB_CONFIG):
     ts_dict = dict()
     for e in cons_dict['short_term']:
         cons_type = e['type']
-        if (cons_type == 'CORRECTED') or (len(e['values']) == 0):
-            continue
-        df = pd.DataFrame(e['values'])
-        df.start_date = pd.to_datetime(df.start_date, utc=True).dt.tz_localize(None)
-        df.end_date = pd.to_datetime(df.end_date, utc=True).dt.tz_localize(None)
-        df['tenor'] = (df.end_date - df.start_date).apply(lambda x: duration_isoformat(x))
-        ts_dict[cons_type] = df
+
+        if cons_type == 'REALISED':
+            if (cons_type == 'CORRECTED') or (len(e['values']) == 0):
+                continue
+            df = pd.DataFrame(e['values'])
+            df.start_date = pd.to_datetime(df.start_date, utc=True).dt.tz_localize(None)
+            df.end_date = pd.to_datetime(df.end_date, utc=True).dt.tz_localize(None)
+            df['tenor'] = (df.end_date - df.start_date).apply(lambda x: duration_isoformat(x))
+    
+            if len(df[df.tenor == '-PT45M'].index.values) != 0:
+                remove_index = [df[df.tenor == '-PT45M'].index.values[0] + i for i in range(-2,2)]
+                df = df.drop(index=remove_index)
+    
+            if len(df[df.tenor == 'PT1H15M'].index.values) != 0:
+                start = df[df.tenor == 'PT1H15M'].iloc[0]['start_date']
+                val = df[df.tenor == 'PT1H15M'].iloc[0]['value']
+                df = df.drop(index=df[df.tenor == 'PT1H15M'].index.values)
+                added_lines = pd.DataFrame([{'start_date' : start + pd.Timedelta(minutes=15*i),
+                                            'end_date' : start + pd.Timedelta(minutes=15*(i+1)),
+                                            'value' : val,
+                                            'tenor' : 'PT15M'} for i in range(5)])
+                df = pd.concat([df, added_lines])
+    
+            ts_dict[cons_type] = df
 
 
     conn = psycopg2.connect(**DB_CONFIG)
